523f11f4c7f5e88e65a1c521c0dcde5a5396c5c6
Steven Yee <s.yee615@gmail.com>
2020-03-08 16:25:43 -0400

    W8D1 Hw...

---
+++ W8D1/app/models/toy.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint           not null, primary key
+#  name         :string           not null
+#  toyable_id   :bigint
+#  toyable_type :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Toy < ApplicationRecord
+  validates :name, uniqueness: { scope: [:toyable] }
+  
+  belongs_to :toyable, polymorphic: true
+
+
+end
